e0496f4cac5a2c79c077c41f9b219d1ad208790a
kasei <i@kasei.wang>
2015-06-22 20:08:19 +0800

    add CPath.PrintScreen()

--- CPath.h
+++ CPath.h
@@ -13,5 +13,33 @@ public:
         iHop = vecPathDistence.size();
         iLength = (*vecPathDistenceInput.end())[1];
     }
-
+    CPath(CPath &x)
+    {
+        iHop = x.iHop;
+        iLength = x.Length;
+        vecPathDistence = x.vecPathDistence;
+    }
+    int getHop()
+    {
+        return iHop;
+    }
+    int getLength()
+    {
+        return iLength;
+    }
+    void PrintScreen()
+    {
+        int i;
+        cout << "Hop:" << iHop << endl;
+        cout << "Length" << iLength << endl;
+        cout << "Path:" ;
+        for(i=0; i<iHop; ++i) {
+            cout << vecPathDistence[i][0] ;
+        }
+        cout << endl << "Distence:" ;
+        or(i=0; i<iHop; ++i) {
+            cout << vecPathDistence[i][1] ;
+        }
+        cout << endl;
+    }
 }
