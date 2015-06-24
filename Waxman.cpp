a5a3863ac6f4602e84240449eba2f41e241351ed
kasei <i@kasei.wang>
2015-06-24 12:08:51 +0800

    add more argv to waxman

--- Waxman.cpp
+++ Waxman.cpp
@@ -3,28 +3,34 @@ using namespace std;
 
 int main(int argc,char *argv[])
 {
+    unsigned int nMatrixSize,nVertexNum;
     double alpha,beta;
+    string strFile;
     if(argc == 1)
     {
+        cout << "MatrixSize:" ;
+        cin >> nMatrixSize ;
+        cout << "VertexNum:" ;
+        cin >> nVertexNum ;
         cout << "Alpha:" ;
         cin >> alpha ;
         cout << "Beta:" ;
         cin >> beta ;
-    }
-    else if(argc > 3 || argc==2 )
-    {
-    	   cerr <<"Useage: "<<argv[0]<<" [Alpha] [Beta] \n \t or Run "<<argv[0]<<" Directly."<<endl;
-    	   return 0;
+        cout << "OutoutFile:" ;
+        cin >> strFile ;
     }
     else
     {
-        alpha = atof(argv[1]);
-        beta = atof(argv[2]);
+        nMatrixSize = atoi(argv[1]);
+        nVertexNum = atoi(argv[2]);
+        alpha = atof(argv[3]);
+        beta = atof(argv[4]);
+        string strOutput(argv[5]);
+        strFile = strOutput;
     }
-    CGraph xxx(100,100, alpha ,beta );
+    CGraph xxx( nMatrixSize, nVertexNum, alpha, beta );
     cout << "结点数：" << xxx.getVertex() << endl;
     cout << "边数：" << xxx.getEdge() << endl;
-    xxx.getFile("output.txt");
-    xxx.getLingoFile("lingo.txt");
+    xxx.getFile(strFile);
     return 0;
 }
